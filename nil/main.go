547bb94e03fdb227e0eefbb85d40c0a001c89ae6
YUTA SHIMAKAWA <banana.umai@gmail.com>
2020-04-07 11:09:36 +0900

    misterious nil

---
+++ nil/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type (
+	i interface {
+		foo()
+	}
+	s struct {}
+)
+
+func (s *s) foo() {}
+
+func main() {
+	vI := cI()
+	fmt.Printf("vI(cI) : %v\n", vI)
+	if vI != nil {
+		fmt.Printf("not nil !? vI(cI) : %v\n", vI)
+	} else {
+		fmt.Printf("yes nil -- vI(cI) : %v\n", vI)
+	}
+
+	vI = cS()
+	fmt.Printf("vI(cS) : %v\n", vI)
+	if vI != nil {
+		fmt.Printf("not nil !? vI(cS) : %v\n", vI)
+	} else {
+		fmt.Printf("yes nil -- vI(cS) : %v\n", vI)
+	}
+
+	vS := cS()
+	fmt.Printf("vS : %v\n", vS)
+	if vS != nil {
+		fmt.Printf("not nil !? vS : %v\n", vS)
+	} else {
+		fmt.Printf("yes nil -- vS : %v\n", vS)
+	}
+}
+
+func cS() *s {
+	return nil
+}
+
+func cI() i {
+	return nil
+}
